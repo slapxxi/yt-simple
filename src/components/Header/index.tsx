0cc176a9a197e53c09f08653ad6088958bb02c34
Slava Pavlutin <sl.pavlutin@gmail.com>
2026-07-13 15:03:28 +0300

    header full width

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,37 +9,42 @@ import { Avatar, SearchInput } from '~/components'
 
 export const Header = () => {
   return (
-    <header className="sticky top-0 z-10 grid grid-cols-subgrid self-start bg-xzinc-900/70 py-5 backdrop-blur-lg area-[header]">
-      <div className="flex gap-7">
-        <MenuIcon className="w-5" />
-        <Logo className="w-29" />
-      </div>
+    <header
+      className="sticky top-0 z-10 grid grid-cols-subgrid self-start 
+    bg-xzinc-900/70 py-5 backdrop-blur-lg col-[full]"
+    >
+      <div className="col-[header] grid grid-cols-subgrid">
+        <div className="flex gap-7">
+          <MenuIcon className="w-5" />
+          <Logo className="w-29" />
+        </div>
 
-      <div className="grid grid-cols-[minmax(auto,830px)_1fr] gap-2 md:gap-10">
-        <SearchInput />
+        <div className="grid grid-cols-[minmax(auto,830px)_1fr] gap-2 md:gap-10">
+          <SearchInput />
 
-        <div
-          className="grid grid-flow-col grid-cols-[0_0_0_40px] items-center gap-0 justify-self-end
-          *:cursor-pointer *:hover:opacity-60 md:grid-cols-[27px_21px_24px_40px] md:gap-10"
-        >
-          <button>
-            <CamIcon />
-          </button>
-          <button>
-            <GridIcon />
-          </button>
-          <button className="col-3 row-1">
-            <BellIcon />
-          </button>
           <div
-            className="col-3 row-1 hidden size-5 translate-x-1/2 -translate-y-1/2 place-items-center rounded-full
-          bg-red-500  text-xs text-white md:inline-grid"
+            className="grid grid-flow-col grid-cols-[0_0_0_40px] items-center gap-0 justify-self-end
+    *:cursor-pointer *:hover:opacity-60 md:grid-cols-[27px_21px_24px_40px] md:gap-10"
           >
-            <span className="-translate-x-px">3</span>
+            <button>
+              <CamIcon />
+            </button>
+            <button>
+              <GridIcon />
+            </button>
+            <button className="col-3 row-1">
+              <BellIcon />
+            </button>
+            <div
+              className="col-3 row-1 hidden size-5 translate-x-1/2 -translate-y-1/2 place-items-center rounded-full
+    bg-red-500  text-xs text-white md:inline-grid"
+            >
+              <span className="-translate-x-px">3</span>
+            </div>
+            <Link to="/user">
+              <Avatar src={userImage} />
+            </Link>
           </div>
-          <Link to="/user">
-            <Avatar src={userImage} />
-          </Link>
         </div>
       </div>
     </header>
